574b3798821dd8afbb690bf9f92bc11169600205
Jayaprakash Das <jayprakashdas5763@gmail.com>
2025-06-07 02:12:42 +0530

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js";  // Import main component
-import "./styles/index.css";  // Import global styles (if any)
+import App from "./App.js"; // Import main component
+import "./styles/index.css"; // Import global styles (if any)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
